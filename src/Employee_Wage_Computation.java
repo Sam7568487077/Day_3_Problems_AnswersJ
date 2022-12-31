2eda5cd86fee664bb0e34abf6cd7185fd05ab5be
Sam K Varghese <ksam071@gmail.com>
2022-12-31 18:41:05 +0530

    UC 1 Program to Check Employee is

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -1,6 +1,16 @@
 public class Employee_Wage_Computation {
     public static void main(String[] args) {
-        System.out.println(" ==== Welcome to Employee Wage Computation ==== "
+        System.out.println("===== Welcome to salary Computation ======");
 
+        int Is_Full_Time=1;
+        double empcheck =Math.floor(Math.random()*10)%2;
+        if(Is_Full_Time == empcheck)
+        {
+            System.out.println(" Employee is present");
+        }
+        else
+        {
+            System.out.println(" Employer is absent");
+        }
     }
 }
