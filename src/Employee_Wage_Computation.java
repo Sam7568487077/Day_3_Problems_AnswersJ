32accbb1caa8149c1b9b41b7e280438fd2097297
Sam K Varghese <ksam071@gmail.com>
2022-12-31 18:47:32 +0530

    Program to calculate Employee Daily Wage

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -1,16 +1,20 @@
 public class Employee_Wage_Computation {
     public static void main(String[] args) {
-        System.out.println("===== Welcome to salary Computation ======");
+
 
         int Is_Full_Time=1;
+        int wage_per_hour=20;
+        int fullday=8;
         double empcheck =Math.floor(Math.random()*10)%2;
         if(Is_Full_Time == empcheck)
         {
             System.out.println(" Employee is present");
+            System.out.print(" Daily wage of employee is: " + wage_per_hour * fullday + "Rs");
         }
         else
         {
             System.out.println(" Employer is absent");
+            System.out.print(" Daily wage of employee is: 0 Rs");
         }
     }
 }
