9f2125a767800dfacd01fb23b7924245c0e6420f
Sam K Varghese <ksam071@gmail.com>
2022-12-31 18:10:59 +0530

    UC 2 Program to check equality of lines

--- src/Line_Comparison_Computation.java
+++ src/Line_Comparison_Computation.java
@@ -1,15 +1,32 @@
 public class Line_Comparison_Computation {
 
     static Math math;
-
     public static void main(String[] args) {
-        double x1=2;
-        double y1=7;
-        double x2=8;
-        double y2=9;
+        double x1 = 2;
+        double y1 = 2;
+        double x2 = 5;
+        double y2 = 5;
+        var z1 =  math.pow((x2 - x1),2);
+        var z2 = math.pow((y2 - y1),2);
+        double a = 5;
+        double b = 4;
+        double c = 9;
+        double d = 3;
+        var a1 =  math.pow(( a - b),2);
+        var a2 = math.pow((c - d),2);
         // Calculate Length between two points
-        double length = Math.sqrt((math.pow((x2 - x1),2) + Math.pow((y2 - y1),2)));
-        System.out.println("Distance between two points :" +length);
+        double length = Math.sqrt(z1 + z2);
+        double length1 = Math.sqrt(a1 + a2);
+        System.out.println("length between two points :" + length);
+        System.out.println("length1 between two points :" + length1);
+        if(length == length1)
+        {
+            System.out.println("Both lines are equal");
+        }
+        else
+        {
+            System.out.println("Both lines are not equal");
+        }
     }
 
 }
