4add1c9685029abd8b8d42e87b8cce9b6d6c95bc
Sam K Varghese <ksam071@gmail.com>
2022-12-31 18:53:45 +0530

    UC 3 Adding Part Time Employee Details

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -1,15 +1,22 @@
 public class Employee_Wage_Computation {
     public static void main(String[] args) {
-
+        System.out.println("===== Welcome to salary Computation ======");
 
         int Is_Full_Time=1;
+        int Is_Half_Time=2;
         int wage_per_hour=20;
         int fullday=8;
-        double empcheck =Math.floor(Math.random()*10)%2;
+        int halfday=4;
+        double empcheck =Math.floor(Math.random()*10)%3;
         if(Is_Full_Time == empcheck)
         {
             System.out.println(" Employee is present");
             System.out.print(" Daily wage of employee is: " + wage_per_hour * fullday + "Rs");
+        }
+        else if ( Is_Half_Time == empcheck)
+        {
+            System.out.println(" Employee is present but has worked half day");
+            System.out.print(" Daily wage of employee is: " + wage_per_hour * halfday + "Rs");
         }
         else
         {
@@ -18,3 +25,4 @@ public class Employee_Wage_Computation {
         }
     }
 }
+
