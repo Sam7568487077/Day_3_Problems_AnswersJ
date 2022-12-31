d9617e1f403497e4d0ac4df2b4ced33505497543
Sam K Varghese <ksam071@gmail.com>
2022-12-31 19:00:27 +0530

    UC 4 Solving using switch case

--- src/Employee_Wage_Computation.java
+++ src/Employee_Wage_Computation.java
@@ -1,14 +1,17 @@
+
+import java.util.Random;
 public class Employee_Wage_Computation {
     public static void main(String[] args) {
         System.out.println("===== Welcome to salary Computation ======");
 
-        int Is_Full_Time=1;
-        int Is_Half_Time=2;
+
         int wage_per_hour=20;
         int fullday=8;
         int halfday=4;
-        double empcheck =Math.floor(Math.random()*10)%3;
-        if(Is_Full_Time == empcheck)
+        int overhead=3;
+        Random sc = new Random();
+        int empcheck =sc.nextInt(overhead);
+      /*  if(Is_Full_Time == empcheck)
         {
             System.out.println(" Employee is present");
             System.out.print(" Daily wage of employee is: " + wage_per_hour * fullday + "Rs");
@@ -22,7 +25,21 @@ public class Employee_Wage_Computation {
         {
             System.out.println(" Employer is absent");
             System.out.print(" Daily wage of employee is: 0 Rs");
+        }*/
+        switch (empcheck) {
+            case 1 -> {
+                System.out.println(" Employee is present");
+                System.out.print(" Daily wage of employee is: " + wage_per_hour * fullday + "Rs");
+            }
+            case 2 -> {
+                System.out.println(" Employee is present but has worked half day");
+                System.out.print(" Daily wage of employee is: " + wage_per_hour * halfday + "Rs");
+            }
+            default -> {
+                System.out.println(" Employer is absent");
+                System.out.print(" Daily wage of employee is: 0 Rs");
+            }
         }
+
     }
 }
-
